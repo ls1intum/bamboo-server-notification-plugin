baa15c98755156461d3aafabab1cc294dbd2bff7
Stefan Klöss-Schuster <kloessst@gmail.com>
2020-05-08 21:36:58 +0200

    Added support for sending artifacts

--- src/main/java/de/tum/in/www1/bamboo/server/ServerNotificationTransport.java
+++ src/main/java/de/tum/in/www1/bamboo/server/ServerNotificationTransport.java
@@ -1,6 +1,11 @@
 package de.tum.in.www1.bamboo.server;
 
+import com.atlassian.bamboo.artifact.MutableArtifact;
 import com.atlassian.bamboo.build.BuildLoggerManager;
+import com.atlassian.bamboo.build.artifact.ArtifactLink;
+import com.atlassian.bamboo.build.artifact.ArtifactLinkDataProvider;
+import com.atlassian.bamboo.build.artifact.ArtifactLinkManager;
+import com.atlassian.bamboo.build.artifact.FileSystemArtifactLinkDataProvider;
 import com.atlassian.bamboo.build.logger.BuildLogger;
 import com.atlassian.bamboo.chains.ChainResultsSummary;
 import com.atlassian.bamboo.chains.ChainStageResult;
@@ -39,12 +44,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ServerNotificationTransport implements NotificationTransport
 {
@@ -63,7 +75,9 @@ public class ServerNotificationTransport implements NotificationTransport
     @Nullable
     private final BuildLoggerManager buildLoggerManager;
 
-    private VariableDefinitionManager variableDefinitionManager = (VariableDefinitionManager) ContainerManager.getComponent("variableDefinitionManager"); // Will be injected by Bamboo
+    // Will be injected by Bamboo
+    private VariableDefinitionManager variableDefinitionManager = (VariableDefinitionManager) ContainerManager.getComponent("variableDefinitionManager");
+    private ArtifactLinkManager artifactLinkManager = (ArtifactLinkManager) ContainerManager.getComponent("artifactLinkManager");;
 
     // Maximum length for the feedback text. The feedback will be truncated afterwards
     private static int FEEDBACK_DETAIL_TEXT_MAX_CHARACTERS = 5000;
@@ -244,7 +258,10 @@ public class ServerNotificationTransport implements NotificationTransport
 
                             jobDetails.put("id", buildResultsSummary.getId());
 
-                            logToBuildLog("Loading cached test results");
+                            logToBuildLog("Loading artifacts for job " + buildResultsSummary.getId());
+                            JSONObject artifacts = createArtifactJSONObjectForJob(buildResultsSummary.getProducedArtifactLinks(), buildResultsSummary.getId());
+
+                            logToBuildLog("Loading cached test results for job " + buildResultsSummary.getId());
                             TestResultsContainer testResultsContainer = ServerNotificationRecipient.getCachedTestResults().get(buildResultsSummary.getPlanResultKey().toString());
                             if (testResultsContainer != null) {
                                 logToBuildLog("Tests results found");
@@ -279,6 +296,79 @@ public class ServerNotificationTransport implements NotificationTransport
 
         logToBuildLog("JSON object created");
         return jsonObject;
+    }
+
+    private String fileToString(Path path) {
+        try {
+            // Use convenience methods to read in the files as reports are not expected to be large
+            return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            logErrorToBuildLog("Error reading in artifact file " + path.toString() + e.getMessage());
+            return null;
+        }
+    }
+
+    private JSONObject createArtifactJSONObject(Path path) {
+        try {
+            JSONObject artifactJSON = new JSONObject();
+            File artifactFile = path.toFile();
+
+            artifactJSON.put("content", fileToString(path));
+            artifactJSON.put("name", artifactFile.getName());
+            artifactJSON.put("path", path.toString());
+            return artifactJSON;
+        } catch (JSONException e) {
+            log.error("JSON construction error for " + path.toString(), e);
+            return new JSONObject();
+        }
+    }
+
+    private JSONArray createFileSystemArtifactJSONArray(File rootFile) {
+        // Travers the file system starting at the rootFile and create a JSONObject for each regular file encountered
+        try (Stream<Path> paths = Files.walk(rootFile.toPath())) {
+                Collection<JSONObject> artifactsForCopyPattern = paths.filter(Files::isRegularFile)
+                        .map(this::createArtifactJSONObject).collect(Collectors.toList());
+                return new JSONArray(artifactsForCopyPattern);
+        } catch (IOException e) {
+            log.error("Error occurred traversing file system for " + rootFile.getName(), e);
+            return new JSONArray();
+        }
+    }
+
+    private JSONObject createArtifactJSONObjectForJob(Collection<ArtifactLink> artifactLinks, long jobId) throws JSONException {
+        JSONObject jobArtifacts = new JSONObject();
+        // ArtifactLink is an interface referring to a single artifact configuration defined on job level
+        for (ArtifactLink artifactLink: artifactLinks) {
+            MutableArtifact artifact = artifactLink.getArtifact();
+            ArtifactLinkDataProvider dataProvider = artifactLinkManager.getArtifactLinkDataProvider(artifact);
+
+            if (dataProvider == null) {
+                log.debug("ArtifactLinkDataProvider is null for " + artifact.getLabel() + " in job " + jobId);
+                logToBuildLog("Could not retrieve data for artifact " + artifact.getLabel() + " in job " + jobId);
+                continue;
+            }
+
+            /*
+             *  Only handles artifact files stored on the server file system
+             *  Has to be extended for more advanced or customized artifact handling
+             */
+            if (dataProvider instanceof FileSystemArtifactLinkDataProvider) {
+                FileSystemArtifactLinkDataProvider fileDataProvider = (FileSystemArtifactLinkDataProvider) dataProvider;
+                File rootFile = fileDataProvider.getFile();
+
+                /**
+                 * Each artifact definition specifies a location and copy pattern to determine which files to keep
+                 * The rootFile is a directory if the copy pattern matches multiple files, otherwise it is a regular file
+                 */
+                jobArtifacts.put(artifact.getLabel(), createFileSystemArtifactJSONArray(rootFile));
+            } else {
+                log.debug("Unsupported ArtifactLinkDataProvider " + dataProvider.getClass().getSimpleName()
+                        + " encountered for label" + artifact.getLabel() + " in job " + jobId);
+                logToBuildLog("Unsupported artifact handler configuration encountered for artifact "
+                        + artifact.getLabel() + " in job " + jobId);
+            }
+        }
+        return jobArtifacts;
     }
 
     private JSONObject createTestsResultsJSONObject(TestResults testResults, boolean addErrors) throws JSONException {
