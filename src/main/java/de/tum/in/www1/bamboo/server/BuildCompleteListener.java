03f90baca25588ba78fdf2649decbdcce74bbcfa
Simon Leiß <git@simon-leiss.de>
2020-08-26 10:55:32 +0200

    Fix error with null tests.

--- src/main/java/de/tum/in/www1/bamboo/server/BuildCompleteListener.java
+++ src/main/java/de/tum/in/www1/bamboo/server/BuildCompleteListener.java
@@ -4,15 +4,17 @@ import com.atlassian.bamboo.v2.build.CurrentBuildResult;
 import com.atlassian.bamboo.v2.build.events.PostBuildCompletedEvent;
 import com.atlassian.event.api.EventListener;
 
+import java.util.Collections;
+
 public class BuildCompleteListener {
 
     @EventListener
     public void onPostBuildComplete(final PostBuildCompletedEvent postBuildCompletedEvent) {
         CurrentBuildResult currentBuildResult = postBuildCompletedEvent.getContext().getBuildResult();
         TestResultsContainer testResultsContainer = new TestResultsContainer(postBuildCompletedEvent.getPlanResultKey(),
-                currentBuildResult.getSuccessfulTestResults(),
-                currentBuildResult.getSkippedTestResults(),
-                currentBuildResult.getFailedTestResults());
+                currentBuildResult.getSuccessfulTestResults() != null ? currentBuildResult.getSuccessfulTestResults() : Collections.emptySet(),
+                currentBuildResult.getSkippedTestResults() != null ? currentBuildResult.getSkippedTestResults() : Collections.emptySet(),
+                currentBuildResult.getFailedTestResults() != null ? currentBuildResult.getFailedTestResults() : Collections.emptySet());
         ServerNotificationRecipient.getCachedTestResults().put(postBuildCompletedEvent.getPlanResultKey().toString(), testResultsContainer);
 
         // Remove old TestResultsContainer based on their initialization timestamp
