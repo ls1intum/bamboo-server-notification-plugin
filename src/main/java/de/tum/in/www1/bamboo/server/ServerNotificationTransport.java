bceeb8e2f3652a12108595e814e053935512e366
Ivan Chimeno <chimeno.ivan@gmail.com>
2021-07-05 18:47:07 +0200

    Prevent static code analysis parser from parsing non-supported reports (#20)

--- src/main/java/de/tum/in/www1/bamboo/server/ServerNotificationTransport.java
+++ src/main/java/de/tum/in/www1/bamboo/server/ServerNotificationTransport.java
@@ -412,12 +412,13 @@ public class ServerNotificationTransport implements NotificationTransport {
              * Has to be extended to support other artifact handling configurations.
              */
             if (dataProvider instanceof FileSystemArtifactLinkDataProvider) {
-
-                // TODO: check for artifact names to match checkstyle or pmd or pmd_cpd or spotbugs and "swift lint" and only try to read in xml files
-
                 FileSystemArtifactLinkDataProvider fileDataProvider = (FileSystemArtifactLinkDataProvider) dataProvider;
-                Optional<JSONObject> optionalReport = createStaticCodeAnalysisReportJSONObject(fileDataProvider.getFile(), artifact.getLabel());
-                optionalReport.ifPresent(artifactJSONObjects::add);
+                File artifactFile = fileDataProvider.getFile();
+
+                if (StaticCodeAnalysisUtils.isStaticCodeAnalysisArtifact(artifact.getLabel(), artifactFile.getName())) {
+                    Optional<JSONObject> optionalReport = createStaticCodeAnalysisReportJSONObject(artifactFile, artifact.getLabel());
+                    optionalReport.ifPresent(artifactJSONObjects::add);
+                }
             }
             else {
                 log.debug("Unsupported ArtifactLinkDataProvider " + dataProvider.getClass().getSimpleName()
---
+++ src/main/java/de/tum/in/www1/bamboo/server/StaticCodeAnalysisUtils.java
@@ -0,0 +1,29 @@
+package de.tum.in.www1.bamboo.server;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class StaticCodeAnalysisUtils {
+
+    /**
+     * Returns whether the specified filename is a file supported by the
+     * static code analysis parser.
+     *
+     * @param artifactLabel the label of the artifact
+     * @param artifactFilename the file name of the artifact file.
+     * @return true if the file is supported by the static code analysis parser.
+     */
+    public static boolean isStaticCodeAnalysisArtifact(String artifactLabel, String artifactFilename) {
+        // The static code analysis parser only supports xml files.
+        if (!FilenameUtils.getExtension(artifactFilename).equals("xml")) {
+            return false;
+        }
+
+        for (StaticCodeAnalysisArtifacts staticCodeAnalysisArtifact : StaticCodeAnalysisArtifacts.values()) {
+            if (staticCodeAnalysisArtifact.toString().equalsIgnoreCase(artifactLabel)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
