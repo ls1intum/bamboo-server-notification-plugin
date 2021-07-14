dc06d58e5fcb7c965b36cba82ee7702bb6391aeb
Alexander Stephan <alexander.stephan@tum.de>
2021-07-14 13:28:49 +0200

    Revert to just xml

--- src/main/java/de/tum/in/www1/bamboo/server/StaticCodeAnalysisUtils.java
+++ src/main/java/de/tum/in/www1/bamboo/server/StaticCodeAnalysisUtils.java
@@ -8,13 +8,13 @@ public class StaticCodeAnalysisUtils {
      * Returns whether the specified filename is a file supported by the
      * static code analysis parser.
      *
-     * @param artifactLabel the label of the artifact
+     * @param artifactLabel    the label of the artifact
      * @param artifactFilename the file name of the artifact file.
      * @return true if the file is supported by the static code analysis parser.
      */
     public static boolean isStaticCodeAnalysisArtifact(String artifactLabel, String artifactFilename) {
-        // The static code analysis parser only supports xml files and json files (just for parsing gcc output).
-        if (!(FilenameUtils.getExtension(artifactFilename).equals("xml") || FilenameUtils.getExtension(artifactFilename).equals("json"))) {
+        // The static code analysis parser only supports xml files.
+        if (!FilenameUtils.getExtension(artifactFilename).equals("xml")) {
             return false;
         }
 
